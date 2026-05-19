20b08ae05ed59bd354dd0079e9fb7d5f1b9bd99d
Qing Zhao <30481137+QingZhaoQE@users.noreply.github.com>
2026-05-19 08:22:08 -0600

    Appendix 2.5_CORRECTION.R

--- Appendix 2.5_CORRECTION.R
+++ Appendix 2.5_CORRECTION.R
@@ -196,11 +196,9 @@ dist_cov_dynam_occu_mcmc <- function(y, x, w, d, nmcmc) {
       } # i
     } # t
     mh1 <- sum(dbinom(z[,-1], 1, z[,-nyear] * phi + (1 - z[,-nyear]) * gamma_star, log=TRUE)) + 
-           dnorm(log(kappa_star), log_kappa_mean, log_kappa_sd, log=TRUE) + 
-           log(kappa_star) # Jacobian adjustment
+           dnorm(log(kappa_star), log_kappa_mean, log_kappa_sd, log=TRUE)
     mh2 <- sum(dbinom(z[,-1], 1, z[,-nyear] * phi + (1 - z[,-nyear]) * gamma     , log=TRUE)) + 
-           dnorm(log(kappa)     , log_kappa_mean, log_kappa_sd, log=TRUE) + 
-           log(kappa     ) # Jacobian adjustment
+           dnorm(log(kappa     ), log_kappa_mean, log_kappa_sd, log=TRUE)
     mh <- exp(mh1 - mh2)
     if (mh > runif(1)) {
       kappa <- kappa_star
